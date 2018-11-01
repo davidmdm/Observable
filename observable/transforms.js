9af96e548793dfed57b8e3a6769fc1976c91a57c
davidmdm <david.desmarais-michaud@pj.ca>
2018-10-31 21:50:28 -0400

    added some transforms

--- observable/transforms.js
+++ observable/transforms.js
@@ -10,4 +10,46 @@ const append = suffix => (value, done) => {
   done(value + suffix);
 };
 
-module.exports = { greaterThan, append };
+const tap = fn => (value, done) => {
+  fn(value);
+  done(value);
+};
+
+const filter = fn => (value, done) => {
+  if (filter(value)) {
+    done(value);
+  }
+};
+
+const delay = ms => (value, done) => {
+  setTimeout(done, ms, value);
+};
+
+const debounceTime = ms => {
+  let timeout;
+  let gate = -1;
+
+  return (value, done) => {
+    const now = Date.now();
+    if (now > gate) {
+      gate = now + ms;
+      return done(value);
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      done(value);
+      gate = Date.now() + ms;
+    }, gate - now);
+  };
+};
+
+const take = j => {
+  let i = 0;
+  return (value, done) => {
+    if (i++ < j) {
+      done(value);
+    }
+  };
+};
+
+module.exports = { greaterThan, append, tap, delay, debounceTime, take };
--- test.js
+++ test.js
@@ -1,24 +1,34 @@
 'use strict';
 
-const { Observable, greaterThan, append } = require('./observable');
+const { Observable, greaterThan, append, debounceTime, take } = require('./observable');
+
+// Observable.create(observer => {
+//   observer.next(5);
+//   observer.next(8);
+//   observer.next(1);
+//   observer.next(4);
+//   observer.next(3);
+//   observer.next(9);
+//   observer.next(0);
+//   observer.next(7);
+
+//   observer.complete();
+
+//   observer.next('nothing here will get caught');
+//   observer.next(100);
+// })
+//   .pipe(
+//     greaterThan(5),
+//     append('!!!')
+//   )
+//   .subscribe(console.log);
 
 Observable.create(observer => {
-  observer.next(5);
-  observer.next(8);
-  observer.next(1);
-  observer.next(4);
-  observer.next(3);
-  observer.next(9);
-  observer.next(0);
-  observer.next(7);
-
-  observer.complete();
-
-  observer.next('nothing here will get caught');
-  observer.next(100);
+  let i = 0;
+  setInterval(() => observer.next(i++), 100);
 })
   .pipe(
-    greaterThan(5),
-    append('!!!')
+    debounceTime(1000),
+    take(3)
   )
   .subscribe(console.log);

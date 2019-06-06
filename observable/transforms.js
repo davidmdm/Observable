e280303afc5be2a9d7b7d27ce97ab9b50f233918
davidmdm <david.desmarais-michaud@pj.ca>
2019-06-06 09:38:04 -0400

    added concatMap, switchMap, exhaustMap, and mergeMap to transforms

--- observable/transforms.js
+++ observable/transforms.js
@@ -1,33 +1,33 @@
 'use strict';
 
-const tap = fn => (value, done) => {
+const tap = fn => (value, next) => {
   fn(value);
-  done(value);
+  next(value);
 };
 
-const filter = fn => (value, done) => {
+const filter = fn => (value, next) => {
   if (filter(value)) {
-    done(value);
+    next(value);
   }
 };
 
-const delay = ms => (value, done) => {
-  setTimeout(done, ms, value);
+const delay = ms => (value, next) => {
+  setTimeout(next, ms, value);
 };
 
 const debounceTime = ms => {
   let timeout;
   let gate = -1;
 
-  return (value, done) => {
+  return (value, next) => {
     const now = Date.now();
     if (now > gate) {
       gate = now + ms;
-      return done(value);
+      return next(value);
     }
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-      done(value);
+      next(value);
       gate = Date.now() + ms;
     }, gate - now);
   };
@@ -35,28 +35,86 @@ const debounceTime = ms => {
 
 const take = j => {
   let i = 0;
-  return (value, done) => {
+  return (value, next) => {
     if (i++ < j) {
-      done(value);
+      next(value);
     }
   };
 };
 
-const map = fn => (value, done) => {
-  done(fn(value));
+const map = fn => (value, next) => {
+  next(fn(value));
 };
 
 const scan = function(fn, seed) {
   let hasSeed = arguments.length > 1;
-  return (value, done) => {
+  return (value, next) => {
     if (!hasSeed) {
       seed = value;
       hasSeed = true;
-      return done(value);
+      return next(value);
     }
     const result = fn(seed, value);
     seed = result;
-    done(result);
+    next(result);
+  };
+};
+
+const mergeMap = fn => (value, next) => {
+  const observable = fn(value);
+  observable.subscribe(next);
+};
+
+const switchMap = fn => {
+  let unsubscribe = null;
+  return (value, next) => {
+    if (unsubscribe) {
+      unsubscribe();
+    }
+    const observable = fn(value);
+    const subscription = observable.subscribe(next);
+    unsubscribe = subscription.unsubscribe.bind(observable);
+  };
+};
+
+const exhaustMap = fn => {
+  let exhausted = true;
+  return (value, next) => {
+    if (!exhausted) {
+      return;
+    }
+    exhausted = false;
+    const observable = fn(value);
+    observable.subscribe({
+      next,
+      complete: () => {
+        exhausted = true;
+      },
+    });
+  };
+};
+
+const concatMap = fn => {
+  // todo use linked list instead of array
+  const queue = [];
+  let active = null;
+
+  const startNextActiveObservable = next => {
+    active = queue.shift();
+    if (!active) {
+      return;
+    }
+    active.subscribe({
+      next,
+      complete: () => startNextActiveObservable(next),
+    });
+  };
+
+  return (value, next) => {
+    queue.push(fn(value));
+    if (!active) {
+      startNextActiveObservable(next);
+    }
   };
 };
 
@@ -67,4 +125,8 @@ module.exports = {
   take,
   map,
   scan,
+  mergeMap,
+  switchMap,
+  exhaustMap,
+  concatMap,
 };
